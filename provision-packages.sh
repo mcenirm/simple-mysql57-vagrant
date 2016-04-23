8fdd6bdfa1b06572db1cc8345eda38acb490386d
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-04-23 16:24:05 -0500

    Import key for MySQL repo

--- provision-packages.sh
+++ provision-packages.sh
@@ -6,4 +6,7 @@ MYSQL_REPO_RPM_NAME=mysql57-community-release-el7-8.noarch
 MYSQL_REPO_RPM_URL=https://repo.mysql.com//${MYSQL_REPO_RPM_NAME}.rpm
 
 rpmquery --quiet "$MYSQL_REPO_RPM_NAME" \
-|| rpm -ivh "$MYSQL_REPO_RPM_URL"
+|| {
+  rpm -ivh "$MYSQL_REPO_RPM_URL"
+  rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
+}
